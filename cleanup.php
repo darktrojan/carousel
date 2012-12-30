45c73a2b34a656e4531c156ef29c541d94de67bf
Geoff Lankow <geoff@darktrojan.net>
2012-12-30 13:54:23 +1300

    Add script to remove unused images

---
+++ cleanup.php
@@ -0,0 +1,12 @@
+<?
+
+$imagePath = './images';
+$order = json_decode(file_get_contents($imagePath.'/order.json'));
+
+$dirHandle = opendir($imagePath);
+while ($file = readdir($dirHandle)) {
+	if (preg_match('/\.(jpg|png)$/', $file) && !in_array($file, $order)) {
+		unlink($imagePath.'/'.$file);
+	}
+}
+closedir($dirHandle);
